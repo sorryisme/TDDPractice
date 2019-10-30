34a263fae4e604cea337b5e96329dcc1e6a7b1d8
sorryisme <wivipp39@gmail.com>
2019-10-31 08:37:45 +0900

    191031 TDD Junit 및 계좌 생성 관련 연습

--- BankBusiness/src/main/Account.java
+++ BankBusiness/src/main/Account.java
@@ -2,4 +2,17 @@ package main;
 
 public class Account {
 
+	private int balance;
+	
+	public Account(int money) {
+		this.balance = money;
+	}
+
+	public Account() {
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
 }
--- BankBusiness/src/test/AccountTest.java
+++ BankBusiness/src/test/AccountTest.java
@@ -1,29 +1,51 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 import main.Account;
+/*
+ * 클래스 이름은 Account : O
+ * 기능은 세가지
+ * 잔고조회
+ * - 10000원,1000원, 0원으로 계좌생성
+ * - 잔고 조회 결과 일치
+ * 입금
+ * 출금
+ * 금액은 원 단위로 (예 천원 =1000)
+ * */
 
 public class AccountTest {
 	
+	@Test
 	public void testAccount() throws Exception {
-		Account account = new Account();
-		
-		if(account == null) {
-			throw new Exception("계좌 생성 불가");
-		}
-		
+		Account account = new Account(10000);
 	}
 	
-	public static void main(String[] args) {
-		AccountTest test = new AccountTest();
+	@Test
+	public void testGetBalanace() throws Exception{
+
+		Account account = new Account(10000);
+		assertEquals(10000,account.getBalance());
 		
-		try {
-			test.testAccount();
-		} catch (Exception e) {
-			System.out.println("실패");
-			return;
-		}
+//		if(account.getBalance() != 10000) {
+//			fail();
+//		}
 		
-		System.out.println("성공");
+		
+		account = new Account(1000);
+		assertEquals(1000,account.getBalance());
+//		if(account.getBalance() != 1000) {
+//			fail();
+//		}
+		
+		account = new Account(0);
+		assertEquals(0,account.getBalance());
+//		if(account.getBalance() != 0) {
+//			fail();
+//		}
+		
 	}
-	
 }
